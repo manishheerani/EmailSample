f1cddf2e9d1068635f2889cd945ce32088c8610a
Manish Heerani <18140536+manishheerani@users.noreply.github.com>
2019-05-16 12:57:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/emailsample/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.emailsample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText recipient;
+    private EditText subject;
+    private EditText body;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recipient = (EditText) findViewById(R.id.recipient);
+        subject = (EditText) findViewById(R.id.subject);
+        body = (EditText) findViewById(R.id.body);
+        Button startBtn = (Button) findViewById(R.id.sendEmail);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendEmail();
+                recipient.setText("");
+                subject.setText("");
+                body.setText("");
+            }
+        });
+    }
+        protected void sendEmail() {
+            String[] recipients = {recipient.getText().toString()};
+            Intent email = new Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
+            // prompts email clients only
+            email.setType("message/rfc822");
+
+            email.putExtra(Intent.EXTRA_EMAIL, recipients);
+            email.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+            email.putExtra(Intent.EXTRA_TEXT, body.getText().toString());
+
+            try {
+                // the user can choose the email client
+                startActivity(Intent.createChooser(email, "Choose an email client from..."));
+
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(MainActivity.this, "No email client installed.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+}
